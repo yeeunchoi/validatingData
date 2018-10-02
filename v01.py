eff0ed58bdcdccb6e456776a6b5d4fc356c3b4ee
yeeunchoi <yc23327@utexas.edu>
2018-10-02 18:04:10 +0900

    adding python file

---
+++ v01.py
@@ -0,0 +1,19 @@
+# open metadata file 
+# chop it and store them in a list 
+numerical = []
+categorical = []
+mixed = []
+# in a text file, write the rule
+# if they are not empty: 
+	# write #numerical rules , categorical rules, mixed rules,
+		# 1. null 
+		# 2. range 
+		# 3. etc
+	# store them in text file 
+
+# Run R script with given data file 
+# Will get a result in a csv file 
+# open a new text file to write the result 
+# formulate a format
+# write the result in the text file 
+# close
